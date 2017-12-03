56350f5c477c21133fd1da6232a2d8be3bc59e7d
Matthew Meyers <m.j.mey3rs@gmail.com>
2017-12-03 12:12:08 -0600

    Updates age of josh text

--- render/format.js
+++ render/format.js
@@ -5,7 +5,7 @@ module.exports = (a, { name, klass, level, paragonLevel, elites, damage }, i) =>
 
   if (i === 0 && name === 'zym') {
     var url = Math.floor(Math.random() * 100) % 2 === 0 ? 'https://i.imgur.com/EMZxF8M.png' : 'https://i.imgur.com/d5FpSNO.png';
-    a = `${url}\n*THE AGE OF JOSH IS UPON US!1!!one!*\n\n`;
+    a = `${url}\n*thE AgE Of jOsh Is UpOn Us!1!!one!*\n\n`;
   }
 
 
